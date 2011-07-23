cb6f668577c3d4ec27dfeb71d8118072b01a8eaf
Benjamin Eberlei <kontakt@beberlei.de>
2011-07-23 17:08:04 +0200

    Explain what the marker interface is about in a doc comment

--- Controller/AutowireController.php
+++ Controller/AutowireController.php
@@ -14,6 +14,15 @@
 
 namespace SimpleThings\DistributionBundle\Controller;
 
+/**
+ * Marker interface for the AutowireListener, controllers of this kind are autoinjected services into methods set*().
+ *
+ * On core controller event a listener finds all `set*()` methods not ending with "Action" and
+ * the variable names are checked against service names by replacing each upper-case character with "." + lower-case char. So
+ * "doctrine.orm.default_entity_manager" is injected into `$doctrineOrmDefault_entity_manager`. Comparison is done lower-case.
+ * Same works for container parameters. Service names are evaluated before parameters, there is no way to use "hidden" parameters.
+ * If neither service nor parameter is found an exception is thrown
+ */
 interface AutowireController
 {
     
